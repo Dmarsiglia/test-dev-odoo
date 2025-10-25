3fb731604821c1d3804c9e0c3fc1c3b25cc61f32
Dmarsiglia <dmarsiglia@bicicletasmilan.com>
2025-10-25 08:50:51 -0500

    Fix .gitignore entries and clean up __manifest__.py formatting

--- kiwiil_addons/motorcycle_financing/__manifest__.py
+++ kiwiil_addons/motorcycle_financing/__manifest__.py
@@ -8,11 +8,3 @@
     "license": "OPL-1",
     "aplication": "True"
 }
-name: Motorcycle Financing
-summary: Streamlines the loan application process for dealerships.
-category: Kawiil/Custom Modules
-version: 18.0.0.0.1
-website: Github Repo URL
-author: Your name or Github User
-license: OPL-1
-application: Tru
