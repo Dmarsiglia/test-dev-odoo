6f27255de2d06884c5226b6041cacf33f1efa616
Dmarsiglia <dmarsiglia@bicicletasmilan.com>
2025-10-24 21:48:52 -0500

    Add initial files for Motorcycle Financing module including .gitignore, manifest, and icon

---
+++ kiwiil_addons/motorcycle_financing/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Motorcycle Financing",
+    "summary": "Streamlines the loan application process for dealerships.",
+    "category": "Kawiil/Custom Modules",
+    "version": "19.0.0.0.0",
+    "website": "Repo GitHub",
+    "author": "Dmarsiglia",
+    "license": "OPL-1",
+    "aplication": "True"
+}
+name: Motorcycle Financing
+summary: Streamlines the loan application process for dealerships.
+category: Kawiil/Custom Modules
+version: 18.0.0.0.1
+website: Github Repo URL
+author: Your name or Github User
+license: OPL-1
+application: Tru
